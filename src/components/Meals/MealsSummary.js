fe6f8caf3d2b246bc1ae21eab238f63dd93a152d
satishsharma <satishs@dewsolutions.in>
2022-10-03 17:58:47 +0530

    Display Meal Item and Adding form

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from '../Meals/MealsSummary.module.css';
+const MealsSummary = () => {
+    return(
+        <section className={classes.summary}>
+            <h2>Delicious Food, Delivered To You</h2>
+            <p>choose your favourate meal from our board selection of available meals and enjoy a 
+                delicious lunch or dinner at home.
+            </p>
+            <p>All our meals are cooked with high quality ingredients , just-in-time and of course by experience chefs.</p>
+        </section>
+    );
+};
+
+export default MealsSummary;
