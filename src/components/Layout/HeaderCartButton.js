0855681e4f89cfc6c6eec62a5445b54e8cac7a01
satishsharma <satishs@dewsolutions.in>
2022-10-07 11:57:47 +0530

    add conditionally bump animation and cleanup function

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,30 @@
-import {useContext} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context'
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnHighLighted] = useState(false)
   const cartCtx = useContext(CartContext)
   const numOfcartItems = cartCtx.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+  const {items} = cartCtx;
+  const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump: ''}`;
+  useEffect(()=>{
+    if (items.length === 0){
+      return;
+    }
+    setBtnHighLighted(true)
+    const timer = setTimeout(()=>{
+      setBtnHighLighted(false);
+    }, 3000)
+    return () => {
+      clearTimeout(timer)
+    }
+  },[items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
